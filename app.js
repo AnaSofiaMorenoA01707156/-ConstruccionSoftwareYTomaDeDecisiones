e8a573e4c2785243afa4eb8e8c2201bf7a2353f8
AnaSofiaMorenoA01707156 <A01707156@tec.mx>
2026-04-18 18:32:58 -0600

    Hacer carpeta de uploads estática para mostrar imágenes subidas por usuarios.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+app.use(express.static(path.join(__dirname, 'public', 'uploads')));
 app.use(express.static(path.join(__dirname, '.', 'public')));
 
 app.set('view engine', 'ejs');
